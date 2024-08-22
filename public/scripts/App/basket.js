1ffec0e93f32f25d7b0f237f2db39a5ec1de28dc
HosseinDev-02 <hosseinDev2002@gmail.com>
2024-08-23 00:48:00 +0330

    update project 11

--- public/scripts/App/basket.js
+++ public/scripts/App/basket.js
@@ -3,7 +3,6 @@ const basketCheckOutBtn = document.getElementById("basket-checkout-btn")
 const basketTotalPrice = document.getElementById("basket-total-price")
 const mobileBasketContainer = document.querySelector('#mobile-basket-list')
 
-import { basketMinusCountAction, basketPlusCountAction, userBasket, getAllBasket } from "./utilities.js";
 
 async function geaAllBaskets() {
     let fetchBaskets = await fetch('https://coffee-shop-6fe4c-default-rtdb.firebaseio.com/baskets.json')
--- public/scripts/App/header.js
+++ public/scripts/App/header.js
@@ -1,4 +1,3 @@
-import { basketMinusCountAction, basketPlusCountAction, headerBasketPriceHandler, totalBasketPrice, userBasket, getAllBasket } from "./utilities.js";
 
 const mobileMenuBtn = document.querySelector("#mobile-menu__btn")
 const mobileMenu = document.querySelector("#mobile-menu")
@@ -11,6 +10,117 @@ const basketBtn = document.getElementById("basket-btn")
 const basketHomeLink = document.getElementById("basket-home-link")
 const basketCount = document.getElementById("basket-count")
 const headerBasketContainer = document.getElementById("header-basket-list")
+
+const totalBasketPrice = document.getElementById("total-basket-price")
+
+async function getAllBasket (){
+    let fetchBaskets = await fetch('https://coffee-shop-6fe4c-default-rtdb.firebaseio.com/baskets.json')
+    let allBaskets = await fetchBaskets.json()
+    if(allBaskets){
+        return Object.entries(allBaskets)
+    }
+}
+const basketMinusCountAction = (basketId, addBasketToDom, basketPriceHandler) => {
+    let allBaskets = getAllBasket()
+        .then(allBaskets => {
+            let mainBasket = allBaskets.find(basket => basket[0] === basketId)
+            let updateBasket = {
+                productId: mainBasket[1].productId,
+                userId: userId,
+                img: mainBasket[1].img,
+                detail: mainBasket[1].detail,
+                costPrice: mainBasket[1].costPrice,
+                price: mainBasket[1].price,
+                count: mainBasket[1].count - 1,
+                checkOut: mainBasket[1].checkOut
+            }
+            if (mainBasket[1].count === 1) {
+                try {
+                    let fetchDeleteBasket = fetch(`https://coffee-shop-6fe4c-default-rtdb.firebaseio.com/baskets/${basketId}.json`, {
+                        method: 'DELETE'
+                    })
+                        .then(result => {
+                            if(result.ok) {
+                                console.log(fetchDeleteBasket)
+                                Swal.fire({
+                                    title: 'محصول مورد نظر حذف شد',
+                                    icon: 'success',
+                                    confirmButton: true,
+                                    cancelButton: true
+                                })
+                                    .then(result => console.log(result))
+                                addBasketToDom()
+                                basketPriceHandler()
+                            }
+                        })
+
+                } catch (err) {
+                    console.log(err, 'مشکلی در حذف محصول از سبد خرید رخ داد')
+                }
+            } else {
+                let fetchUpdateBasket = fetch(`https://coffee-shop-6fe4c-default-rtdb.firebaseio.com/baskets/${basketId}.json`, {
+                    method: 'PUT',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(updateBasket)
+                }).then(result => {
+                    if(result.ok){
+                        addBasketToDom()
+                        basketPriceHandler()
+                    }
+                })
+                console.log(fetchUpdateBasket)
+            }
+        })
+
+
+}
+async function basketPlusCountAction(basketId, addBasketToDom, basketPriceHandler){
+    let allBasket = await getAllBasket()
+    let mainBasket = allBasket.find(basket => basket[0] === basketId)
+
+    let mainBasketPlus = {
+        productId: mainBasket[1].productId,
+        userId: userId,
+        img: mainBasket[1].img,
+        detail: mainBasket[1].detail,
+        costPrice: mainBasket[1].costPrice,
+        price: mainBasket[1].price,
+        count: mainBasket[1].count + 1,
+        checkOut: mainBasket[1].checkOut
+    }
+    let fetchUpdateBasket = await fetch(`https://coffee-shop-6fe4c-default-rtdb.firebaseio.com/baskets/${basketId}.json`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(mainBasketPlus)
+    })
+    console.log(fetchUpdateBasket)
+    await addBasketToDom()
+    await basketPriceHandler()
+}
+const userBasket = () => {
+    let allBaskets = getAllBasket()
+        .then(allBaskets => {
+            if(allBaskets) {
+                return allBaskets.filter(basket => basket[1].userId === userId)
+            }
+        })
+    return allBaskets
+}
+
+const headerBasketPriceHandler = () => {
+    let sumBasketPrice = 0
+    userBasket()
+        .then(baskets => {
+            if(baskets) {
+                baskets.forEach(basket => sumBasketPrice += (basket[1].costPrice * basket[1].count))
+                totalBasketPrice.innerHTML = `${sumBasketPrice.toLocaleString()} تومان `
+            }
+        })
+}
 
 const addHeaderBasketToDom = () => {
     headerBasketContainer.innerHTML = ""
