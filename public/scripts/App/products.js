96aed39e9d763b992d805ebf821231f348ced006
HosseinDev-02 <hosseinDev2002@gmail.com>
2024-08-24 18:25:26 +0330

    update project 14

--- public/scripts/App/products.js
+++ public/scripts/App/products.js
@@ -28,11 +28,44 @@ async function addProductsToDom() {
         allProducts.forEach(product => {
             let newProductCard = document.createElement('div')
             newProductCard.className = 'rounded-lg bg-zinc-900 overflow-hidden'
-            if(product[1].offer === '0'){
-                newProductCard.innerHTML = `<a href="#" class="flex items-center justify-center w-full h-[140px] xs:h-[200px] rounded overflow-hidden"><img class="w-full h-full object-cover" src="${product[1].img}" alt="" loading="lazy"></a><div class="p-2 md:p-3 sm:mb-2"><h4 class="h-10 md:h-12 text-sm md:text-base font-Shabnam-Medium line-clamp-2 mb-5 text-gray-400">${product[1].detail}</h4><div class="flex items-center md:items-end justify-between flex-wrap sm:gap-y-2"><div class="flex flex-col items-end justify-between xs:items-start flex-wrap"><div class="product__price flex items-center gap-x-1 font-Shabnam-Bold text-lg md:text-xl text-white">${product[1].costPrice.toLocaleString()}<span class="text-xs md:text-base font-Shabnam-Regular">تومان</span></div><div class="offer flex items-center gap-x-1.5 font-Shabnam-Bold text-white">${product[1].price.toLocaleString()}<span class="inline text-sm tracking-tighter font-Shabnam-Regular">تومان</span></div></div><div class="hidden md:flex xs:flex-col items-center xs:items-end flex-wrap justify-between gap-y-1 gap-x-1 w-full xs:w-auto"><span class="flex items-center gap-x-1 font-Shabnam-Medium text-xs md:text-sm">${product[1].view}<svg class="w-4 h-4"><use href="#eye"></use></svg></span><div class="flex items-center"><svg class="w-4 md:w-5 h-4 md:h-5"><use href="#star"></use></svg><svg class="w-4 md:w-5 h-4 md:h-5 heart--fill"><use href="#star"></use></svg><svg class="w-4 md:w-5 h-4 md:h-5 heart--fill"><use href="#star"></use></svg><svg class="w-4 md:w-5 h-4 md:h-5 heart--fill"><use href="#star"></use></svg><svg class="w-4 md:w-5 h-4 md:h-5 heart--fill"><use href="#star"></use></svg></div></div></div></div><button onclick='addProductToBasket("${product[0]}")' class="hidden md:flex items-center justify-center hover:text-white transition-colors w-full h-12 bg-indigo-600 font-Shabnam-Medium rounded-md text-sm md:text-base">افزودن به سبد خرید</button>`
-            }else{
-                newProductCard.innerHTML = `<a href="#" class="flex items-center justify-center w-full h-[140px] xs:h-[200px] rounded overflow-hidden"><img class="w-full h-full object-cover" src="${product[1].img}" alt="" loading="lazy"></a><div class="p-2 md:p-3 sm:mb-2"><h4 class="h-10 md:h-12 text-sm md:text-base font-Shabnam-Medium line-clamp-2 mb-5 text-gray-400">${product[1].detail}</h4><div class="flex items-center md:items-end justify-between flex-wrap sm:gap-y-2"><span class="bg-red-500 flex md:hidden items-center justify-center w-8 h-8 rounded-full text-xs font-Shabnam-Bold">${product[1].offer}</span><div class="flex flex-col items-end justify-between xs:items-start flex-wrap"><div class="product__price flex items-center gap-x-1 font-Shabnam-Bold text-lg md:text-xl text-white">${product[1].costPrice.toLocaleString()}<span class="text-xs md:text-base font-Shabnam-Regular">تومان</span></div><div class="offer flex items-center gap-x-1.5 font-Shabnam-Bold text-white">${product[1].price.toLocaleString()}<span class="inline text-sm tracking-tighter font-Shabnam-Regular">تومان</span></div></div><div class="hidden md:flex xs:flex-col items-center xs:items-end flex-wrap justify-between gap-y-1 gap-x-1 w-full xs:w-auto"><span class="flex items-center gap-x-1 font-Shabnam-Medium text-xs md:text-sm">${product[1].view}<svg class="w-4 h-4"><use href="#eye"></use></svg></span><div class="flex items-center"><svg class="w-4 md:w-5 h-4 md:h-5"><use href="#star"></use></svg><svg class="w-4 md:w-5 h-4 md:h-5 heart--fill"><use href="#star"></use></svg><svg class="w-4 md:w-5 h-4 md:h-5 heart--fill"><use href="#star"></use></svg><svg class="w-4 md:w-5 h-4 md:h-5 heart--fill"><use href="#star"></use></svg><svg class="w-4 md:w-5 h-4 md:h-5 heart--fill"><use href="#star"></use></svg></div></div></div></div><button onclick='addProductToBasket("${product[0]}")' class="hidden md:flex items-center justify-center hover:text-white transition-colors w-full h-12 bg-indigo-600 font-Shabnam-Medium rounded-md text-sm md:text-base">افزودن به سبد خرید</button>`
-            }
+            newProductCard.innerHTML = `<a href="#"
+                               class="flex items-center justify-center w-full h-[120px] sm:h-[160px] md:h-[200px] rounded-md overflow-hidden">
+                                <img class="w-full h-full object-cover" src="${product[1].img}" alt=""
+                                     loading="lazy">
+                            </a>
+                            <div class="p-2">
+                                <h4 class="text-sm md:text-base font-IRANSans-Medium line-clamp-2 mb-4 sm:mb-6 text-gray-200 h-10 md:h-12">
+                                    ${product[1].detail}
+                                </h4>
+                                <div class="flex justify-between">
+                                    <div class="flex flex-col justify-between sm:justify-start gap-y-2.5">
+                                        <div class="flex items-center gap-x-1">
+                                            <svg class="w-4 sm:w-5 h-4 sm:h-5 fill-amber-300 shrink-0 text-amber-300">
+                                                <use href="#star"></use>
+                                            </svg>
+                                            <span class="font-IRANSans-Bold text-xs leading-5 sm:leading-[19px] sm:text-sm">
+                                        4.5
+                                    </span>
+                                        </div>
+                                        <div class="flex items-center gap-1 font-IRANSans-Bold text-xs tracking-tighter">
+                                            <svg class="w-4 h-4"><use href="#eye"></use></svg>
+                                            <span class="font-IRANSans">${product[1].view}</span>
+                                        </div>
+                                    </div>
+                                    <div class="flex flex-col justify-between items-start sm:items-end flex-wrap gap-x-4">
+                                        <div class="product__price flex items-center gap-x-1 font-IRANSans-Bold text-base tracking-wider md:text-xl text-white">
+                                            ${product[1].price.toLocaleString()}
+                                        </div>
+                                        <div class="offer flex items-center gap-x-1 font-IRANSans-Bold text-base tracking-wider text-gray-400">
+                                            ${product[1].costPrice.toLocaleString()}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <button onclick='addProductToBasket("${product[0]}")'
+                                    class="flex text-gray-100 sm:mt-0.5 md:text-gray-300 items-center justify-center md:hover:text-white transition-colors w-full h-10 md:h-12 bg-indigo-600 font-IRANSans-Medium rounded-md tracking-tighter text-sm md:text-base">
+                                افزودن به سبد خرید
+                            </button>`
 
             productsFragment.append(newProductCard)
         })
